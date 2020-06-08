c9f32c2a76cde3de9987a232449f61d935f78f99
lpenaud <l.penaud@zaclys.net>
2020-06-08 16:36:26 +0200

    Update helpers/parallel
    * Replace PARALLEL_COUNT variable by parallel::count function wich use
    jobs primitive
    * Remove wait and wait_all function to use directly wait primitive

--- test/parallel.sh
+++ test/parallel.sh
@@ -8,33 +8,32 @@ DIRNAME="$(dirname "${0}")"
 function test_1 () {
   local prefix="test_1"
   local -a com
-  assert_equals 0 "${PARALLEL_COUNT}" "${prefix} - PARALLEL_COUNT expected to be equals 0 at the start"
+  assert_equals 0 "$(parallel::count)" "${prefix} - parallel::count expected to be equals 0 at the start but $(parallel::count)"
   com=(sleep 3)
   parallel::run com
   com=(sleep 1)
   parallel::run com
-  assert_equals 2 "${PARALLEL_COUNT}" "${prefix} - PARALLEL_COUNT expected to be equals 2"
-  parallel::wait_all
-  assert_equals 0 "${PARALLEL_COUNT}" "${prefix} - PARALLEL_COUNT expected to be equals 0 at the end"
+  assert_equals 2 "$(parallel::count)" "${prefix} - parallel::count expected to be equals 2 but $(parallel::count)"
+  wait
+  assert_equals 0 "$(parallel::count)" "${prefix} - parallel::count expected to be equals 0 at the end but $(parallel::count)"
 }
 
 function test_2 () {
   local prefix="test_2"
   local -i idt1 idt2
   local -a com
-  assert_equals 0 "${PARALLEL_COUNT}" "${prefix} - PARALLEL_COUNT expected to be equals 0 at the start"
+  assert_equals 0 "$(parallel::count)" "${prefix} - parallel::count expected to be equals 0 at the start but $(parallel::count)"
   com=(sleep 10)
   parallel::run com idt1
   com=(sleep 2)
   parallel::run com idt2
-  assert_equals 2 "${PARALLEL_COUNT}" "${prefix} - PARALLEL_COUNT expected to be equals 2"
+  assert_equals 2 "$(parallel::count)" "${prefix} - parallel::count expected to be equals 2 but $(parallel::count)"
   kill -kill "${idt1}"
-  parallel::wait "${idt1}" "${idt2}"
-  # Same as parallel::wait_all
-  assert_equals 0 "${PARALLEL_COUNT}" "${prefix} - PARALLEL_COUNT expected to be equals 0 at the end"
+  wait "${idt1}" "${idt2}"
+  assert_equals 0 "$(parallel::count)" "${prefix} - parallel::count expected to be equals 0 at the end but $(parallel::count)"
 }
 
 parallel::init
 test_1
 test_2
-
+wait
