0bf3a077088d7d2a4a5d4711f12852f95061988f
lpenaud <l.penaud@zaclys.net>
2020-12-16 00:52:47 +0100

    flac: Add featuring format

--- multimedia/flac/format-flac.sh
+++ multimedia/flac/format-flac.sh
@@ -11,7 +11,8 @@ function format_flac::read_tags () {
   local -n metadata=$1
   local -r infile="${2}"
   local -a tags
-  local line
+  local -i i
+  local line tag key
   shift 2
   while [ $# -ne 0 ]; do
     tags+=("--show-tag=${1}")
@@ -19,22 +20,35 @@ function format_flac::read_tags () {
   done
   while read -e line; do
     if [[ "${line}" =~ ^([A-Za-z]+)=(.+)$ ]]; then
-      metadata["${BASH_REMATCH[1]}"]="${BASH_REMATCH[2]}"
+      i=0
+      tag="${BASH_REMATCH[1]}"
+      # In case thes keys have duplicates (don't try this at home, it's not clean at all)
+      while [ -n "${metadata[${tag}]}" ]; do
+        (( i++ ))
+        tag="${BASH_REMATCH[1]}${i}"
+      done
+      metadata["${tag}"]="${BASH_REMATCH[2]}"
     fi
-  done < <(metaflac ${tags[@]} "${infile}")
+  done < <(metaflac "${tags[@]}" "${infile}")
+  if [ -n "${metadata[ARTIST1]}" ]; then
+    if key="$(array::find_key metadata "${metadata[ALBUMARTIST]}")"; then
+      metadata["${key}"]="${metadata[ARTIST]}"
+      metadata["ARTIST"]="${metadata[ALBUMARTIST]}"
+    fi
+  fi
 }
 
 # tag
 function format_flac::get_tag_format () {
   case "${1}" in
     TRACKNUMBER)
-      echo -n "%02d"
+      echo "%02d"
       ;;
     DISCNUMBER)
-      echo -n "%d"
+      echo "%d"
       ;;
     *)
-      echo -n "%s"
+      echo "%s"
       ;;
   esac
 }
@@ -43,10 +57,23 @@ function format_flac::get_tag_format () {
 function format_flac::format_from_template () {
   local -n metadata=$1
   local -a values
+  local -i i
   local rematch="${2}" result="${2%%\%*}"
   while [[ "${rematch}" =~ %([A-Z]+)(.*)$ ]]; do
-    result+="$(format_flac::get_tag_format "${BASH_REMATCH[1]}")${BASH_REMATCH[2]%%\%*}"
-    values+=("${metadata[${BASH_REMATCH[1]}]}")
+    if [ "${BASH_REMATCH[1]}" = "ARTISTS" ]; then
+      i=0
+      result+="$(format_flac::get_tag_format ARTIST)"
+      values+=("${metadata[ARTIST${i}]}")
+      while [ -n "${metadata[ARTIST${i}]}" ]; do
+        result+=", $(format_flac::get_tag_format ARTIST)"
+        values+=("${metadata[ARTIST${i}]}")
+        (( i++ ))
+      done
+    else
+      result+="$(format_flac::get_tag_format "${BASH_REMATCH[1]}")"
+      values+=("${metadata[${BASH_REMATCH[1]}]}")
+    fi
+    result+="${BASH_REMATCH[2]%%\%*}"
     rematch="${BASH_REMATCH[2]}"
   done
   printf -v result "${result}" "${values[@]}"
@@ -54,11 +81,16 @@ function format_flac::format_from_template () {
   echo "${result//[<>:\"\/\\|\?\*]/_}"
 }
 
-# tracks_directory, [track='%TRACKNUMBER - %TITLE'], [playlist='00 - %ALBUM'], [directory='%DATE %ALBUM']
+# tracks_directory,
+# [track='%TRACKNUMBER - %TITLE'],
+# [playlist='00 - %ALBUM'],
+# [directory='%DATE %ALBUM']
+# [featuring='%TRACKNUMBER - %TITLE (feat. %ARTISTS)']
 function format_names () {
   local -r track="${2:-%TRACKNUMBER - %TITLE}"
-  local -r directory="${4:-%DATE %ALBUM}"
   local -r playlist="${3:-00 - %ALBUM}"
+  local -r directory="${4:-%DATE %ALBUM}"
+  local -r featuring="${5:-%TRACKNUMBER - %TITLE (feat. %ARTISTS)}"
   local -A infos
   local -a tracks
   local f filename
@@ -76,7 +108,11 @@ function format_names () {
     if [[ "${infos[TRACKNUMBER]}" =~ ^0([0-9]+)$ ]]; then
       infos[TRACKNUMBER]="${BASH_REMATCH[1]}"
     fi
-    filename="$(format_flac::format_from_template infos "${track}").flac"
+    if [ -n "${infos[ARTIST1]}" ]; then
+      filename="$(format_flac::format_from_template infos "${featuring}").flac"
+    else
+      filename="$(format_flac::format_from_template infos "${track}").flac"
+    fi
     mv "${f}" "${1}/${filename}"
   done
 
@@ -103,12 +139,13 @@ function format_flac::usage () {
 function format_flac::help () {
   format_flac::usage
   help::description "Format flac filesname."
-  help::positional_argument "FLAC_DIRECTORY" "Directory with flac file on it."
+  help::positional_argument "FLAC_DIRECTORY" "directory with flac file on it"
   help::optional
   help::optional_help
-  help::optional_argument "-t" "--track" var="TRACK" desc="Format of track filename." default="%TRACKNUMBER - %TITLE"
-  help::optional_argument "-p" "--playlist" var="PLAYLIST" desc="Format of playlist filename." default="00 - %ALBUM"
-  help::optional_argument "-d" "--directory" var="DIRECTORY" desc="Format of directory filename." default="%DATE %ALBUM"
+  help::optional_argument "-t" "--track" var="TRACK" desc="format of track filename" default="%TRACKNUMBER - %TITLE"
+  help::optional_argument "-p" "--playlist" var="PLAYLIST" desc="format of playlist filename" default="00 - %ALBUM"
+  help::optional_argument "-d" "--directory" var="DIRECTORY" desc="format of directory filename" default="%DATE %ALBUM"
+  help::optional_argument "-f" "--featuring" var="FEATURING" desc="the format of the track, if the track has more than one artist" default="%TRACKNUMBER - %TITLE (feat. %ARTISTS)"
 }
 
 if [ $# -lt 1 ]; then
@@ -119,7 +156,7 @@ fi
 
 declare -a directories cmd
 declare -i code errno=0
-declare track playlist directory
+declare track playlist directory featuring
 while [ $# -ne 0 ]; do
   case "${1}" in
     -t | --track)
@@ -134,6 +171,9 @@ while [ $# -ne 0 ]; do
       directory="${2}"
       shift
       ;;
+    -f | --featuring)
+      featuring="${2}"
+      ;;
     -h | --help)
       format_flac::help
       exit 0
@@ -142,7 +182,7 @@ while [ $# -ne 0 ]; do
       if [ -d "${1}" ]; then
         directories+=("${1}")
       else
-        echo "TRACKS_DIRECTORY '${1}' must be a directory" >&2
+        style::warning "Ignore this argument, it's not a directory: '${1}'" >&2
       fi
       ;;
   esac
@@ -156,6 +196,7 @@ for d in "${directories[@]}"; do
   cmd[2]="${track}"
   cmd[3]="${playlist}"
   cmd[4]="${directory}"
+  cmd[5]="${featuring}"
   parallel::run cmd
 done
 wait
