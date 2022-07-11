27c0f23b9fd83ef286422ad291349f73d1ae0fa2
lpenaud <l.penaud@zaclys.net>
2022-07-11 22:27:11 +0200

    Update git-config script

--- git/git-config.sh
+++ git/git-config.sh
@@ -1,5 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-SCRIPT=`realpath $0`
-SCRIPTPATH=`dirname $SCRIPT`
-cp --verbose $SCRIPTPATH/.gitconfig $HOME/
+declare -r FILENAME="$(realpath "${BASH_SOURCE[0]}")"
+declare -r DIRNAME="$(dirname "${FILENAME}")"
+cp --verbose "${DIRNAME}/.gitconfig" "${HOME}/.gitconfig"
+
+cat >> ~/.bashrc <<EOF
+if [ -f "${DIRNAME}/bash-git-prompt/gitprompt.sh" ]; then
+    GIT_PROMPT_ONLY_IN_REPO=1
+    source "${DIRNAME}/bash-git-prompt/gitprompt.sh"
+fi
+EOF
